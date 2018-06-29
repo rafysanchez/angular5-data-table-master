b6e0f7ab7700ccfa79db0a71d91771251f71fa69
rafysanchez <rafysanchez@hotmail.com>
2018-06-29 15:02:32 -0300

    listagem
    sdasdsa

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,10 +6,14 @@ import {User} from "./User";
 export class ApiService {
 
   public getUsers(): Observable<any> {
-    let fakeUsers : User[] = [{position: 1, firstName: 'Dhiraj', lastName: 'Ray', email: 'dhiraj@gmail.com'},
+    const fakeUsers: User[] = [
+      {position: 1, firstName: 'Dhiraj', lastName: 'Ray', email: 'dhiraj@gmail.com'},
       {position: 2, firstName: 'Tom', lastName: 'Jac', email: 'Tom@gmail.com'},
       {position: 3, firstName: 'Hary', lastName: 'Pan', email: 'hary@gmail.com'},
-      {position: 4, firstName: 'praks', lastName: 'pb', email: 'praks@gmail.com'},
+      {position: 4, firstName: 'xser', lastName: 'pb', email: 'xxx@gmail.com'},
+      {position: 5, firstName: 'rtey', lastName: 'Jac', email: 'www@gmail.com'},
+      {position: 6, firstName: 'uyio', lastName: 'Pan', email: 'ttt@gmail.com'},
+      {position: 7, firstName: 'nhyui', lastName: 'pb', email: 'yyy@gmail.com'}
     ];
     return Observable.of(fakeUsers).delay(500);
   }
